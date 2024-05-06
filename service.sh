fa2c2d929d1ab6b5d354324fc46c3a802f525811
ZephyrTFA <matthew@tfaluc.com>
2024-05-06 05:16:13 -0400

    exec instead

--- service.sh
+++ service.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-if [ -f "service.pid" ]; then
-    kill -9 $(cat service.pid)
-    rm service.pid
-fi
 git pull
-cargo run --release > service.log 2>&1 &
-echo $! > service.pid
-fg
+exec cargo run --release
